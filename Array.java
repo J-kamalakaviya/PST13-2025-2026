dfe4a3bb6ce07936442953f190a57cab762f1b22
J-kamalakaviya <152888494+J-kamalakaviya@users.noreply.github.com>
2026-02-03 14:09:39 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,30 @@
+package PST;
+import java.util.Scanner;
+public class Array {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the size: ");
+		int size = sc.nextInt();
+		
+		int arr[] = new int[size];
+		System.out.println("enter array:");
+		
+		for(int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+			
+		
+			
+			System.out.println("Array elements are:");
+		
+	        for (int i = 0; i < size; i++) {
+	            System.out.print(arr[i] + " ");
+	        }
+	  
+	       sc.close();
+	        
+	   }
+		
+	}
+
+	
