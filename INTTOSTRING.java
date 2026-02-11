8a01eff122c277de956d4a812f7e392ed2b18de1
J-kamalakaviya <152888494+J-kamalakaviya@users.noreply.github.com>
2026-02-11 12:10:41 +0530

    Add files via upload

---
+++ INTTOSTRING.java
@@ -0,0 +1,20 @@
+package PST;
+import java.util.Scanner;
+public class INTTOSTRING {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a int:");
+		int num = sc.nextInt();
+		A1 obj = new A1();
+		obj.AA1(num);
+		
+	}
+  
+}
+class A1{
+	void AA1() {
+		
+		System.out.println("int to string:");
+	}
+
+}
