d04958ab18ed37486f6217fa8981d2e184a30e59
J-kamalakaviya <152888494+J-kamalakaviya@users.noreply.github.com>
2026-02-11 12:15:52 +0530

    Add files via upload

---
+++ CharacterCount.java
@@ -0,0 +1,13 @@
+package PST;
+import java.util.Scanner;
+public class CharacterCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("Characters = " + str.length());
+        sc.close();
+    }
+       
+}
+
+
