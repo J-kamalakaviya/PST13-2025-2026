30ebd12fe9c8ae12d2bfb375e6b7aa5c27631456
J-kamalakaviya <152888494+J-kamalakaviya@users.noreply.github.com>
2026-02-11 12:01:40 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+package PST;
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		method();
+		
+		
+	}
+	public static void method() {
+		System.out.println("she is good");
+		method1();
+		method2();
+	}
+	public static void method1() {
+		System.out.println("she is mental");
+	}
+	public static void method2() {
+		System.out.println("she is loosu");
+	}
+	
+
+}
