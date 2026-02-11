742a3f3f699116687a5f387864c336e3d80802c2
J-kamalakaviya <152888494+J-kamalakaviya@users.noreply.github.com>
2026-02-11 12:14:19 +0530

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,74 @@
+package PST;
+public class Class{
+	public static void main(String[] args){
+		CSEA obj=new CSEA();
+		obj.method1();
+		
+		CSEB obj1=new CSEB();
+		obj1.method2();
+		
+		
+		 
+    }
+}
+
+class CSEA{
+
+	public static void method1(){
+		
+		System.out.println("madhu");
+		method1a();
+		method1b();
+		method1c();
+		 
+	}
+	
+		
+	
+	public static void method1a(){
+		
+		System.out.println("deva");
+		}
+	public static void method1b(){
+		System.out.println("dharshu");
+		 
+	}
+	public static void method1c(){
+		System.out.println("kaviya");
+		 
+	}
+	
+}
+	
+class CSEB{
+	public static void method2(){
+		System.out.println("ahisha");
+			method2a();
+			method2b();
+			method2c();
+			 
+		}
+	public static void method2a(){
+		System.out.println("kiruthi");
+	}
+	public static void method2b(){
+		System.out.println("varsha");
+			 
+	}
+	public static void method2c(){
+		System.out.println("sakthi");
+			 
+	    }	
+	}
+	
+
+
+ 
+	 
+
+		
+		
+	
+
+
+
